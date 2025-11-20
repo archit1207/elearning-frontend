8c6af35d1bb4af74343f2ef546d9634e0243c949
archit1207 <156530958+archit1207@users.noreply.github.com>
2025-11-20 21:50:06 +0530

    Connect Backend Server Link

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import  { UserContextProvider } from './context/UserContext';
 import { CourseContextProvider } from './context/CourseContext.jsx';
 
-export const server = 'http://localhost:5000';
+export const server = 'https://elearning-server-5ii5.onrender.com/';
 
 
 createRoot(document.getElementById('root')).render(
