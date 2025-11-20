0a9becfbee964d308f1bda727dbac3973ab33c38
archit1207 <architmishra1207@gmail.com>
2025-11-20 23:01:37 +0530

    Updated Web Page title and Logo

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import  { UserContextProvider } from './context/UserContext';
 import { CourseContextProvider } from './context/CourseContext.jsx';
 
-export const server = "https://elearning-server-5ii5.onrender.com/";
+export const server = "https://elearning-server-5ii5.onrender.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
