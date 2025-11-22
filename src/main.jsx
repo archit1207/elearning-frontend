9ffbca5ffdd5a2ddad44ed073b7901d0cd6617ad
archit1207 <architmishra1207@gmail.com>
2025-11-22 16:39:55 +0530

    Change server Link

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+export const server = "https://elearning-server-5ii5.onrender.com";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import  { UserContextProvider } from './context/UserContext';
 import { CourseContextProvider } from './context/CourseContext.jsx';
 
-export const server = "https://elearning-server-5ii5.onrender.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
